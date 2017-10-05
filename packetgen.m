38f605b600fcb11afb5313b930197c4573b27969
jimin <jimin@Desktop>
2017-10-04 19:26:01 -0700

    ssh nothing happened

--- packetgen.m
+++ packetgen.m
@@ -1,8 +1,8 @@
 function out = packetgen(lambda)
-    r = 0 + (0.2).*rand(lambda,1);
+    r = 0 + (0.2).*rand(lambda * 10,1);
     X = (-1/lambda).*log(1-r);
     Y = ceil(X./0.00002);
-    out = zeros(1, lambda);
+    out = zeros(1, lambda * 10);
     for i = 1:lambda
         sum = 0;
         for j = 1:i
