93cb314abb9d437e4011b0458a173a44990f41d6
jimin <jimin@Desktop>
2017-10-04 19:35:35 -0700

    why did it push those last changes

--- packetgen.m
+++ packetgen.m
@@ -1,8 +1,8 @@
 function out = packetgen(lambda)
-    r = 0 + (0.2).*rand(lambda * 10,1);
+    r = 0 + (0.2).*rand(lambda,1);
     X = (-1/lambda).*log(1-r);
     Y = ceil(X./0.00002);
-    out = zeros(1, lambda * 10);
+    out = zeros(1, lambda);
     for i = 1:lambda
         sum = 0;
         for j = 1:i
